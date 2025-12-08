012d74e7f667d762d245c8adaad6b5e6cd9ee89e
Igor Niedbała <igor_niedbala@op.pl>
2025-12-08 16:04:30 +0100

    Refactor color definitions and tree printing logic

--- x-mass-tree.c
+++ x-mass-tree.c
@@ -8,8 +8,8 @@
 int size;
 char light = 'o';
 
-const char* colors[] ={"\e[1;33m", "\e[0;34m", "\e[0;35m", "\e[0;36m","\e[0;31m", "\e[0;37m", "\e[0;32m"};
-//const char* colors[] ={"\e[1;32m", "\e[0;31m", "\e[0;37m"};
+//const char* colors[] ={"\e[1;33m", "\e[0;34m", "\e[0;35m", "\e[0;36m","\e[0;31m", "\e[0;37m", "\e[0;32m"};
+const char* colors[] ={"\e[1;32m", "\e[0;31m", "\e[0;37m"};
 int main(){
     struct winsize w;
 
@@ -38,16 +38,31 @@ int main(){
         printf("%s", spaces);
 
         //print the trees
+        //print the rest of the tree
         for (int y = 0; y < size; y++){
+            int radnomForSnow = rand() % ((size - y) - 0 + 1) + 0;
             for (int s = size - y; s>=0; s--){
-                printf(" ");
+                if(s == radnomForSnow){
+                    printf("\e[0;37m.\e[0;0m");
+                }else{
+                    printf(" ");
+                }
             }
             for (int x = 0; x < y; ++x){
                 number = rand() % (max - min + 1) + min;
-                if((y != random && (y % 2 == 0 && x%2 == 0)) || (x != random && x % 3 == 0 && y % 2 == 0)){
+                if(y == 1){
+                    printf("\e[0;33m# \e[0;0m");
+                }else if((y != random && (y % 2 == 0 && x%2 == 0)) || (x != random && x % 3 == 0 && y % 2 == 0)){
                     printf("%s%c \e[0;0m",colors[number], light);
                 }else{
                     printf("\e[0;32m* \e[0;0m");
+                }
+            }
+            for (int s = size - y; s>=0; s--){
+                if(s == radnomForSnow){
+                    printf("\e[0;37m.\e[0;0m");
+                }else{
+                    printf(" ");
                 }
             }
             printf("\n");
@@ -59,7 +74,7 @@ int main(){
             for (int s = size/2 + 1*size/4; s >= 0; s--){
                 printf(" ");
             }
-            for (int j = 0; j < size/3; j++){
+            for (int j = 0; j < size/4; j++){
                 printf("\e[0;31m* \e[0;0m");
             }
             printf("\n");
@@ -73,16 +88,31 @@ int main(){
         printf("%s", spaces);
 
         //print the trees
+        //print the rest of the tree
         for (int y = 0; y < size; y++){
+            int radnomForSnow = rand() % ((size - y) - 0 + 1) + 0;
             for (int s = size - y; s>=0; s--){
-                printf(" ");
+                if(s == radnomForSnow){
+                    printf("\e[0;37m.\e[0;0m");
+                }else{
+                    printf(" ");
+                }
             }
             for (int x = 0; x < y; ++x){
                 number = rand() % (max - min + 1) + min;
-                if((y != random && y % 2 == 0 && x%2 == 0) || (x != random && x % 3 == 0 && y % 2 == 0)){
+                if(y == 1){
+                    printf("\e[0;33m# \e[0;0m");
+                }else if((y != random && y % 2 == 0 && x%2 == 0) || (x != random && x % 3 == 0 && y % 2 == 0)){
                     printf("%s%c \e[0;0m",colors[number], light);
                 }else{
                     printf("\e[0;32m* \e[0;0m");
+                }
+            }
+            for (int s = size - y; s>=0; s--){
+                if(s == radnomForSnow){
+                    printf("\e[0;37m.\e[0;0m");
+                }else{
+                    printf(" ");
                 }
             }
             printf("\n");
@@ -94,7 +124,7 @@ int main(){
             for (int s = size/2 + 1*size/4; s >= 0; s--){
                 printf(" ");
             }
-            for (int j = 0; j < size/3; j++){
+            for (int j = 0; j < size/4; j++){
                 printf("\e[0;31m* \e[0;0m");
             }
             printf("\n");
